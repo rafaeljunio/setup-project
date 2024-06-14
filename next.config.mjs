d42d10418702b5d6a97f820710fecfde7f402bc1
Rafael Souza <rafael.souza@iexfy.com>
2024-06-14 20:35:22 -0300

    feat: setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' *;",
+          },
+        ],
+      },
+    ]
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({ test: /\.node$/, use: 'raw-loader' })
 
-export default nextConfig;
+    if (!isServer) config.externals.push('canvas')
+    return config
+  },
+}
+
+export default nextConfig
